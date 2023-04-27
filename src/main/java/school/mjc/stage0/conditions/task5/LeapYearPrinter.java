c3adf27ff48b65ecfe74d5b973a6616d4b5405e0
Aleksandr Uzienko <a.uzienko@gmail.com>
2023-04-27 17:19:16 +0300

    solution

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -1,6 +1,11 @@
 package school.mjc.stage0.conditions.task5;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        System.out.println(
+                year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ? "leap" : "not leap"
+        );
     }
 }
